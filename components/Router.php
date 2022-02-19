ff84e8084417cd9db8b3543f18422d255fb38b5a
Janysh Saliev <Janysh Saliev@DESKTOP-PT5SA9E>
2022-02-19 21:58:16 +0300

    Lesson1 - commit 2

--- components/Router.php
+++ components/Router.php
@@ -8,23 +8,73 @@ class Router{
         $routerPath=ROOT.'/config/routes.php';
         $this->routes=include($routerPath);
     }
+
+    # ПРИВАТНАЯ ФУНКЦИЯ ВОЗВРАШАЮШИЙ URI В ВИДЕ СТРОКИ
+    private function getURI(){
+        if(!empty($_SERVER['REQUEST_URI'])){
+            return trim($_SERVER['REQUEST_URI'],'/');
+        }
+    }
     
     public function run(){
-        // ПОЛУЧИТЬ СТРОКУ ЗАПРОСА
-        if(!empty($_SERVER['REQUEST_URI'])){
-            $uri=trim($_SERVER['REQUEST_URI'],'/');
-        }
         
-        echo $uri;
+        // ПОЛУЧИТЬ СТРОКУ ЗАПРОСА
+        $uri=$this->getURI();
         
         // ПРОВЕРКА НАЛИЧИЕ ТАКОГО ЗАПРОСА
-        
-        
-        // ЕСЛИ ЕСТЬ СОВПАДЕНИЕ, ОПРЕДЕЛИТЬ КАКОЙ КОНТРОЛЛЕР
-        // И ACTION ОБРАБАТЫВАЮТ ЗАПРОС
-        
-        
-        // ПОДКЛЮЧИТЬ ФАЙЛ КЛАССА-КОНТРОЛЛЕРА
+        foreach($this->routes as $uriPattern=>$path){
+
+            // ЕСЛИ ЕСТЬ СОВПАДЕНИЕ
+            # СРАВНЕНИЕ $uriPattern И $uri
+            if (preg_match("~$uriPattern~",$uri)) {
+                /*
+                echo $path;
+                echo $uriPattern;
+                */
+
+                //ОПРЕДЕЛИТЬ КАКОЙ КОНТРОЛЛЕР И ACTION ОБРАБАТЫВАЮТ ЗАПРОС
+                $segments=explode('/',$path);
+                /*
+                echo "<pre>";
+                print_r($segments);
+                echo "</pre>";
+                */
+
+                # ИЗВЛЕКАЕМ ПЕРВЫЙ ЭЛЕМЕНТ МАССИВА
+                # СОЕДИНЯЕМ К НЕМУ СТРОКУ 'Controller' 
+                # И ПРИСВАЕВАЕМ К ПЕРЕМЕННОЙ $controllerName
+                $controllerName=array_shift($segments).'Controller';
+
+                # УВЕЛИЧИВАЕМ ПЕРВЫЙ СИМВОЛ В СТРОКЕ И ПЕРЕПРИСВАЕВАЕМ
+                $controllerName=ucfirst($controllerName);
+                //echo $controllerName; //Имя контроллера (класса)
+
+                # ТАК ЖЕ КАК И ВЫШЕ ИЗВЛЕКАЕМ ПЕРВЫЙ ЭЛЕМЕМЕНТ ИЗ ЭЛЕМЕНТОВ ОСТАВШИХСЯ В МАССИВЕ
+                # ДОБАВЛЯЕМ С ПЕРЕДИ СТРОКУ 'action'
+                $actionName='action'.ucfirst(array_shift($segments));
+                //echo $actionName; //Имя action (метода)
+
+                // ПОДКЛЮЧИТЬ ФАЙЛ КЛАССА-КОНТРОЛЛЕРА
+
+                # ОПРЕДЕЛЯЕМ ФАЙЛ, ПУТЬ И НАЗВАНИЕ
+                $controllerFile= ROOT . '/controllers/' .$controllerName. '.php';
+
+                # ПРОВЕРЯЕМ СУШЕСТВУЕТ ЛИ ТАКОЙ ФАЙЛ ПО УКАЗСННОМУ ПУТИ
+                if(file_exists($controllerFile)){
+                    include_once($controllerFile);
+                }
+
+                // СОЗДАТЬ ОБЪЕКТ, ВЫЗВАТЬ МЕТОД (Т.Е. ACTION)
+                $controllerObject = new $controllerName;
+                $result=$controllerObject->$actionName();
+
+                if ($result!=null) {
+                    break;
+                }
+
+            }
+            
+        }
         
         
         // СОЗДАТЬ ОБЪЕКТ, ВЫЗВАТЬ МЕТОД (Т.Е. ACTION)
